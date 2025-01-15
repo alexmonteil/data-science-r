b999d9d3fdc892416198cba5cc8de92b1dac7889
alexmonteil <alex.monteil@outlook.com>
2025-01-15 16:02:44 -0600

    finished data frames notes

--- r-data-frames/r-data-frames.R
+++ r-data-frames/r-data-frames.R
@@ -15,6 +15,7 @@ summary(df)
 df[1,] # Selects entire 1st row
 df[,1] # Selects entire 1st column
 df[2:3,] # Selects rows 2 to 3
+df[-2,] # Returns all rows minus 2nd row (minus sign excludes index, only works with numeric indices)
 df[2:3, 2:3] # Selects entries in rows 2 to 3 and columns 2 to 3
 df[,"rain"] # Selects column with name rain
 df[1:4,c("days", "temps")] # Selects rows 1 - 4 for days and temps columns
@@ -39,12 +40,12 @@ df[sorted_temps_dsc,] # Selects all rows by sorted by temps in descending order
 
 c1 <- 1:10
 c2 <- letters[1:10]
-df <- data.frame(col.name.1 = c1, col.name.2 = c2)
+df2 <- data.frame(col.name.1 = c1, col.name.2 = c2)
 
 # Reading and Writing csv files
 
 write.csv(df, file = "saved_df.csv") # Writes data frame into csv file
-df2 <- read.csv('saved_df.csv') # Read csv file into variable
+df3 <- read.csv('saved_df.csv') # Read csv file into variable
 
 # Counting rows and columns
 
@@ -66,19 +67,52 @@ summary(df)
 
 # Single cell selection
 
-df[[5,2]] # Returns cell at row 5 col 2
-df[[5,2]] <- "z" # Reassigns new value to cell at row 5 col 2
+df2[[5,2]] # Returns cell at row 5 col 2
+df2[[5,2]] <- "z" # Reassigns new value to cell at row 5 col 2
 
 # Row selection
 
 df[1:2,] # Returns rows 1 and 2
+df[-2,] # Returns all rows except row 2
 
 # Column selection
 
 df[,1] # Returns column 1 (days) as vector
+df[,-1] # Returns all columns except column 1
 df$days # Returns column 1 (days) as vector
 df[,"days"] # Returns column 1 (days) as vector
 df["days"] # Returns column 1 (days) as data frame
 
+# Addings rows 
+
+df4 <- data.frame(col.name.1 = 2000, col.name.2 = 'new')
+df2_new <- rbind(df2, df4)
+
+# Adding columns method 1
+
+df2$col.name.3 <- 2 * df2$col.name.1
+
+# Adding columns method 2
+
+col.name.4 <- c(T,T,F,T,F,T,T,F,F,T)
+df2 <- cbind(df2, col.name.4)
+
+# Renaming columns
+
+colnames(df2) <- c("Col 1", "Col 2", "Col 3", "Col 4") # Renames all columns
+colnames(df2)[1] <- "1st Column" # Renames column 1
+
+# Filtering data frames
+
+mtcars[mtcars$mpg > 20,] # Returns all rows where mpg < 20
+mtcars[(mtcars$mpg > 20 & mtcars$cyl == 6),] # Returns all rows where mpg > 20 and cyl == 6
+mtcars[mtcars$mpg > 20, c("mpg", "cyl", "hp")] # Returns mpg, cyl and hp columns and only rows where mpg > 20
+subset(df, subset = mpg > 20 & cyl == 6) # Returns all rows where mpg > 20 and cyl == 6 (other method)
+
+# Dealing with missing data
+
+is.na(mtcars) # Returns a matrix of logicals showing if each element is NA
+any(is.na(mtcars)) # Returns a logical indicating if any entry is NA
+mtcars$mpg[is.na(mtcars$mpg)] <- mean(mtcars$mpg) # Replaces all NA entries in mpg column with mean of mpg column
 
 
