af2274541223c714ec797b18e69d9d63f5480ffa
alexmonteil <alex.monteil@outlook.com>
2025-01-17 10:14:14 -0600

    notes on  r programming basics

---
+++ r-programming-basics/r-programming-basics.R
@@ -0,0 +1,49 @@
+# R Programming Basics
+
+# Logical Operators
+
+# & --> AND
+# |--> OR
+# ! --> NOT
+  
+# Decisions
+
+if (condition) {
+  
+  # Statement here
+  
+} else if (condition2) {
+  
+  # Other Statement
+  
+} else {
+  
+  # Other Statement
+  
+}
+
+# While Loops
+
+while (condition) {
+  
+  # Code executes while condition is true
+  
+}
+
+# For Loops
+
+for (variable in vector) {
+  
+  print(variable)
+  
+}
+
+# Functions
+
+name_of_function <- function(input1,input2) {
+  
+  # Code that needs to execute
+  return(data)
+}
+
+
